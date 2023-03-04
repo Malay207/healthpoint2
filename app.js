ee156c085fd93f16e6b2253f8a85d2a65bc66ac5
malay <malaydev207@gmail.com>
2023-03-04 07:35:43 +0530

    update2 app.js

--- app.js
+++ app.js
@@ -11,8 +11,24 @@ const app = express();
 
 const host = "localhost";
 app.use(bodyparser.json());
-app.use("/", router);
 app.use(express.static(path.join(__dirname, "./healthpoint/build")))
+app.use("/", router);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
